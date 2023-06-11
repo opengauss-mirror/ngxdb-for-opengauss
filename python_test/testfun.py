85247d9dc4a4a894341c164ee3ba9a04598698ee
水之狐 <gmthings@sina.com>
2023-06-11 09:02:25 +0800

    安装练习版

---
+++ python_test/testfun.py
@@ -0,0 +1,135 @@
+import json
+import random
+import threading
+import time
+import sys
+import psycopg2
+import requests
+from base64 import b64encode 
+from datetime import datetime
+
+
+
+url='http://192.168.1.94:80/func/'
+loginname='admin'  #账号1
+passward='123456' #密码1
+conn=psycopg2.connect(database="opengauss", user="gm", password="gao@12345", host="192.168.1.94", port="5432")
+cur=conn.cursor()
+
+
+icount=0
+isucc=0
+testreport=[]
+starttime=datetime.now()
+
+def report(detail=False):
+    global icount,isucc,testreport
+    if detail:
+        for i in range(0,icount):
+            print(testreport[i][0],testreport[i][1],testreport[i][2],testreport[i][3])
+    print("开始时间:%s 结束时间:%s 耗时:%r秒" %(starttime,endtime,format(endtime-starttime)))
+    print("总用例:%i 成功:%i 失败:%i" %(icount,isucc,icount-isucc))
+
+def getfun(funcname,param,report=True):
+    global icount,testreport,isucc
+    re=requests.get(url+funcname,param)
+    testreport
+    try:
+        rejson=re.json()
+    except: 
+        print(re.text)
+        if report:
+            testreport.append([funcname,param,None,False])
+        return None
+    if (rejson["errorcode"]!=0):
+        print(rejson["message"])
+        if report:
+            icount+=1
+            testreport.append([funcname,param,re.text,False])
+    else:
+        if report:
+            isucc=isucc+1
+            icount+=1
+            testreport.append([funcname,param,re.text,True])
+    return rejson
+
+def login(loginname,passw,system=None,report=True):
+    rj=getfun("sysinfo/login",{"loginname":loginname,"pass":passw,"system":system},report)
+    return rj
+
+def test_sysinfo_login():
+    global isucc,testreport,icount,token
+    param={"loginname":"asasf","pass":"sdfsdf"}    
+    for i in range(6):
+        icount+=1
+        rj=getfun("sysinfo/login",param,False)
+        if rj==None:
+            testreport.append(["sysinfo/login",param,None,True])
+        else: 
+            if rj["errorcode"]==100001:
+                isucc=isucc+1
+                testreport.append(["sysinfo/login",param,rj,True])
+            else:
+                testreport.append(["sysinfo/login",param,rj,False])
+    
+    rj=getfun("sysinfo/login",param,False)
+    icount+=1
+    if rj==None:
+        testreport[icount]={"sysinfo/login",param,None,True}
+    else: 
+        if rj["errorcode"]==100002:
+            isucc=isucc+1
+            testreport.append(["sysinfo/login",param,rj,True])
+        else:
+            testreport.append(["sysinfo/login",param,rj,False])
+    time.sleep(60*10)
+    for i in range(5):
+        icount+=1
+        rj=getfun("sysinfo/login",param,False)
+        if rj==None:
+            testreport[icount]={"sysinfo/login",param,None,True}
+        else: 
+            if rj["errorcode"]==100001:
+                isucc=isucc+1
+                testreport.append(["sysinfo/login",param,rj,True])
+            else:
+                testreport.append(["sysinfo/login",param,rj,False])    
+    rj=getfun("sysinfo/login",param,False)
+    icount+=1
+    if rj==None:
+        testreport[icount]={"sysinfo/login",param,None,True}
+    else: 
+        if rj["errorcode"]==100003:
+            isucc=isucc+1
+            testreport.append(["sysinfo/login",param,rj,True])
+        else:
+            testreport.append(["sysinfo/login",param,rj,False])
+
+def test_gm_check_login():
+    global icount,cur,isucc,testreport
+    cur.callproc('gm.check_login',['asdaddd'+token,100])
+    re=cur.fetchall()[0][0]
+    icount+=1
+    if re['errorcode']==100005:
+        isucc+=1
+        testreport.append(['gm.check_login',['asdaddd'+token,100],re,True])
+    else:
+        testreport.append(['gm.check_login',['asdaddd'+token,100],re,False])
+
+    cur.callproc('gm.check_login',[token,100])
+    re=cur.fetchall()[0][0]
+    icount+=1
+    if re['errorcode']==0:
+        isucc+=1
+        testreport.append(['gm.check_login',[token,100],re,True])
+    else:
+        testreport.append(['gm.check_login',[token,100],re,False])
+
+test_sysinfo_login()
+param={"loginname":loginname,"pass":passward}
+rj=getfun("sysinfo/login",param,True)
+token=rj['info']['token']
+test_gm_check_login()
+endtime=datetime.now()
+report(True)
+conn.close
--- readme.md
+++ readme.md
@@ -1,20 +1,127 @@
-NGXDB-FOR-OPENGAUSS
-会sql就能做后端
+# 欢迎使用 NGXDB FOR OPENGAUSS开发套件
 
-本开发框架专为opengauss开发，能完成编写代码、接口文档、单元测试、部署，可以只用纯sql语句完成全套系统从开发到部署，详见示例sql
-本插件基于nginx，需要下载nginx源码编译，编译后在nginx的配置文件里加入
+------
+#----会sql就能做后端
+------
+
+##一、介绍
+    本开发框架为专为opengauss开发，能完成编写代码、接口文档、单元测试、部署，可以只用纯sql语句完成全套系统从开发到部署，完全开源。
+文件目录
+|---extension  opengauss扩展
+&nbsp;---opengauss nginx插件
+&nbsp;---python_test python的测试代码
+##二、安装步骤
+###1、安装opengauss
+详见官网
+*请将opengauss安装目录设为/opt/software/openGauss，以便后续步骤不用修改opengauss依赖```linux
+###2、下载源码
+```linux
+git clone https://gitee.com/opengauss/ngxdb-for-opengauss.git
+```
+###3、nginx编译安装
+下载nginx源码，以nginx24.0为例
+```linux
+wget https://nginx.org/download/nginx-1.24.0.tar.gz
+tar zxf nginx-1.24.0.tar.gz
+cd nginx-1.24.0
+cd src
+cp ../../opengauss .
+cd ..
+./configure --add-module=src/opengauss  
+make & make install
+cd ..
+```
+*如果opengauss的安装目录不是/opt/software/openGauss，会提示有文件找不到，请将src/opengauss/config文件里的/opt/software/openGauss替换为opengauss的安装目录
+*nginx默认安装目录为/usr/local/nginx，安装完nginx后请核对
+###4、安装opengauss扩展示例
+```linux
+cp extension/*.* /opt/software/openGauss/share/postgresql/extension/
+gs_ctl restart
+gsql
+create extension opengauss_login;
+\q
+```
+###5、配置nginx
+```linux
+vi /usr/local/nginx/conf/nginx.conf
+```
+找到如下代码段
+```linux
+    server {
+        listen       80;
+        server_name  localhost;
+
+        #charset koi8-r;
+
+        #access_log  logs/host.access.log  main;
+
+        location / {
+            root   html;
+            index  index.html index.htm;
+        }
+
+        #error_page  404              /404.html;
+```
+插入代码
+```linux
        location /func {
-          opengaussconn "host=127.0.0.1 dbname=opengauss user=conn password=Gao12345 port=5432";
+          opengaussconn "host=127.0.0.1 dbname=postgres user=conn password=Gao12345 port=5432";
           rewrite ^/func/(.*)$ /$1 break;
        }
 
         location /help {
-            opengausshelp "host=127.0.0.1 dbname=opengauss user=conn password=Gao12345 port=5432";
+            opengausshelp "host=127.0.0.1 dbname=postgres user=conn password=Gao12345 port=5432";
             break;
         }
-		
-在opengauss数据库里执行示例sql后，启动nginx。
-在浏览器输入http://127.0.0.1/help 可以看到系统管理相关接口的文档说明
-在浏览器输入http://127.0.0.1/func/sysinfo/login?loginname=admin&pass=123456可以看到系统登录后的token和登录账号相关信息，完成系统登录功能
 
+```
+最终代码为
+```linux
+    server {
+        listen       80;
+        server_name  localhost;
+
+        #charset koi8-r;
+
+        #access_log  logs/host.access.log  main;
+
+       location /func {
+          opengaussconn "host=127.0.0.1 dbname=postgres user=conn password=Gao12345 port=5432";
+          rewrite ^/func/(.*)$ /$1 break;
+       }
+
+        location /help {
+            opengausshelp "host=127.0.0.1 dbname=postgres user=conn password=Gao12345 port=5432";
+            break;
+        }
+
+        location / {
+            root   html;
+            index  index.html index.htm;
+        }
+
+        #error_page  404              /404.html;
+
+```
+在vi中输入wq回车，保存
+运行nginx
+```linux
+/usr/local/nginx/sbin/nginx
+```
+###6、测试
+####使用测试代码
+```linux
+python python_test\testfun.py
+```
+运行大约耗时10分钟，会测试正常登录、token验证、10分钟内超过5次非法登录将锁定账号10分钟，10次非法登录将锁定3小时。
+如果运行完演示
+总用例:16 成功:16 失败:0
+则表示安装成功。
+####使用浏览器
+在本机打开浏览器，在地址栏输入http://127.0.0.1/help
+可以看到后端接口的帮助文档
+在本机打开浏览器，在地址栏输入http://127.0.0.1/sysinfo/login?loginname=admin&pass=123456
+可以看到浏览器返回json字符串，详见帮助文档
+##三、说明
+本版本为0.1，仅供熟悉本框架的初学者练习，下一版本为可实用的版本
 
